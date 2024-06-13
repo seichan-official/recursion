09a44b48c29e222b10c77ee166416468e04620fa
Seiya <seichan_official@SeiyanoMacBook-Air.local>
2024-06-13 21:24:22 +0900

    backend1

--- backend_project1/Guess the number game.py
+++ backend_project1/Guess the number game.py
@@ -2,14 +2,18 @@ import random
 
 number = random.randint(1,10)
 guess = None
-
+attempts = 0
+max_attempts = 5
 while guess != number:
-    guess = input("guess a number between 1 and 10: ")
+    guess = input(f"Attempt {attempts + 1} of {max_attempts}: Guess a number between 1 and 10: ")
     guess = int(guess)
-    
+    attempts += 1
     if guess == number:
-        print("You win!")
+        print(f"You win! It took you {attempts} attempts")
         break
     
     else:
         print("You lose")
+        
+    if attempts == max_attempts:
+        print(f"You've used all {max_attempts} attempts. The correct number was {number}.")
